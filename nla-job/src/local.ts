58859641228af4d8b83aedd0e6536a1a7f1813b7
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-10-12 12:15:18 +0100

    Just call the handler

--- nla-job/src/local.ts
+++ nla-job/src/local.ts
@@ -12,19 +12,5 @@ AWS.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
 AWS.config.sessionToken = process.env.AWS_SESSION_TOKEN;
 AWS.config.region = "eu-west-1";
 
-async function run(event) {
-    handler(event).
-      on('success', function(response) {
-        console.log("Success!");
-      }).
-      on('error', function(response) {
-        console.log("Error!");
-      }).
-      on('complete', function(response) {
-        console.log("Always!");
-      });
-}
 
-run({
-    Records: []
-});
+handler();
