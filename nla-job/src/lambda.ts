897f9309d6bc2b311c54a50eea832ea322f1c176
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2018-10-12 15:10:02 +0100

    Fix credentials thing

--- nla-job/src/lambda.ts
+++ nla-job/src/lambda.ts
@@ -1,51 +1,59 @@
 import {Config} from './config';
 
 let AWS = require('aws-sdk');
-let config = new Config();
+
+const config = new Config();
 
 export function handler() {
-  let sts = new AWS.STS();
-  sts.assumeRole({
+  const chain = new AWS.CredentialProviderChain();
+  chain.providers.unshift(() => new AWS.TemporaryCredentials({
     RoleArn: config.AthenaRole,
     RoleSessionName: 'capi-nla-athena-to-s3'
-  }, (error, data) => {
-    if (error) console.log(error, error.stack);
-    else runQuery(data.Credentials);
-  });
+  }));
+  chain.providers.unshift(() => new AWS.SharedIniFileCredentials({ profile: 'ophan' }))
+  chain.resolve(runQuery);
 }
 
-export function runQuery(creds) {
-  let athena = new AWS.Athena({ ... creds });
-  
-  const yesterday = new Date();
-  yesterday.setDate(yesterday.getDate() - 1);
-  const query = `
-    SELECT web_title, url_raw, pv.path AS url_path, count(1) AS pageviews, received_date
-    FROM   clean.pageview pv
-           INNER JOIN clean.content_new c ON pv.path = c.path
-           CROSS JOIN UNNEST (content_type_tag) AS A (a_type_tag)
-           CROSS JOIN UNNEST (tone_tag) AS A (a_tone_tag)
-    WHERE  received_date = date'${yesterday.toISOString().slice(0, 10)}'
-    AND    platform = 'NEXT_GEN'
-    AND    (a_type_tag = 'type/article' OR a_tone_tag = 'tone/minutebyminute')
-    GROUP BY 1,2,3,5
-    ORDER BY 4 DESC
-  `;
+export function runQuery(err, credentials) {
+  if (err) console.log(err, err.stack);
+  else {
+    let athena = new AWS.Athena({ 
+      region: 'eu-west-1', 
+      accessKeyId: credentials.accessKeyId,
+      secretAccessKey: credentials.secretAccessKey,
+      sessionToken: credentials.sessionToken
+    });
+    
+    const yesterday = new Date();
+    yesterday.setDate(yesterday.getDate() - 1);
+    const query = `
+      SELECT web_title, url_raw, pv.path AS url_path, count(1) AS pageviews, received_date
+      FROM   clean.pageview pv
+            INNER JOIN clean.content_new c ON pv.path = c.path
+            CROSS JOIN UNNEST (content_type_tag) AS A (a_type_tag)
+            CROSS JOIN UNNEST (tone_tag) AS A (a_tone_tag)
+      WHERE  received_date = date'${yesterday.toISOString().slice(0, 10)}'
+      AND    platform = 'NEXT_GEN'
+      AND    (a_type_tag = 'type/article' OR a_tone_tag = 'tone/minutebyminute')
+      GROUP BY 1,2,3,5
+      ORDER BY 4 DESC
+    `;
 
-  const params = {
-    QueryString: query,
-    ResultConfiguration: {
-      OutputLocation: config.Destination,
-    },
-    ClientRequestToken: yesterday.toString(),
-    QueryExecutionContext: {
-      Database: config.Schema
-    }
-  };
+    const params = {
+      QueryString: query,
+      ResultConfiguration: {
+        OutputLocation: config.Destination,
+      },
+      ClientRequestToken: yesterday.toString(),
+      QueryExecutionContext: {
+        Database: config.Schema
+      }
+    };
 
-  return athena.startQueryExecution(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
+    return athena.startQueryExecution(params, function(err, data) {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    });
+  }
 }
 
--- nla-job/src/local.ts
+++ nla-job/src/local.ts
@@ -1,16 +1,4 @@
 import { handler } from './lambda';
 let AWS = require('aws-sdk');
 
-/**
- * For testing locally:
- * `yarn run local <source bucket> <object key>`
- */
-
-AWS.config = new AWS.Config();
-AWS.config.accessKeyId = process.env.AWS_ACCESS_KEY_ID;
-AWS.config.secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
-AWS.config.sessionToken = process.env.AWS_SESSION_TOKEN;
-AWS.config.region = "eu-west-1";
-
-
 handler();
