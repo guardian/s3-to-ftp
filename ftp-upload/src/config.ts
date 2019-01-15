3d205a2011f6b2caa3c043b7185adc13d9fc71af
Regis Kuckaertz <regis.kuckaertz@theguardian.com>
2019-01-15 16:50:14 +0000

    Remove ZipFile option

--- ftp-upload/src/config.ts
+++ ftp-upload/src/config.ts
@@ -2,6 +2,5 @@ export class Config {
     FtpHost: string = process.env.FtpHost;
     FtpUser: string = process.env.FtpUser;
     FtpPassword: string = process.env.FtpPassword;
-    ZipFile: boolean = process.env.ZipFile.toLowerCase() === "true";
     AthenaRole: string = process.env.AthenaRole;
 }
